36a8b34e9f04a5c92eca28bf7c17a1916b0574c8
mybezen <amriikhda207@gmail.com>
2024-11-07 11:17:29 +0700

    Add fe startup

---
+++ fe-masjidque/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="flex items-center justify-center bg-slate-500 w-full min-h-screen">
+      <h1 className="font-bold text-3xl text-pink-500">MASJIDQUE</h1>
+    </div>
+  )
+}
+
+export default App
