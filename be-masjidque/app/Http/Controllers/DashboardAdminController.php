f566ab58f0ed75718fd01753151f1439dd78ff9f
awiipp <sayidariipp@gmail.com>
2025-01-07 14:20:51 +0700

    add SuperAdmin controller.

--- be-masjidque/app/Http/Controllers/DashboardAdminController.php
+++ be-masjidque/app/Http/Controllers/DashboardAdminController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
-use function PHPUnit\Framework\isEmpty;
-
 class DashboardAdminController extends Controller
 {
     public function index(Request $request)
